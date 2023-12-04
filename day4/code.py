40c95612f965ea5a6430b90bda67a7d8d6506172
bocasper2001 <boyd.caspers@nielsen.com>
2023-12-04 19:17:31 +0100

    day4-update

--- day4/code.py
+++ day4/code.py
@@ -2,18 +2,6 @@ from collections import defaultdict
 data = open("day4/data.txt", "r").read().splitlines()
 
 # Part 1
-res = 0
-for s in data:
-    a, b = s.split(': ')[1].split(' | ')
-    a = [int(x) for x in a.split(' ') if x != '']
-    b = [int(x) for x in b.split(' ') if x != '']
-    n = len(set(a).intersection(set(b)))
-    if n:
-        res += 2**(n-1)
-
-print(f'Part 1: {res}')
-       
-# Part 2
 card_matches = {}
 for i, s in enumerate(data):
     a, b = s.split(': ')[1].split(' | ')
@@ -22,6 +10,9 @@ for i, s in enumerate(data):
     n = len(set(a).intersection(set(b)))
     card_matches[i+1] = n
 
+print(f'Part 1: {sum([2**(v-1) for v in card_matches.values() if v > 0])}')
+       
+# Part 2
 total_cards = {k: 1 for k in range(1, len(data)+1)}
 for i in range(1, len(data) + 1):
     for wins in range(1, card_matches[i] + 1):
